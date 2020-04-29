638d7a11d0928e7a01c07e805e5a4c67c26cc603
sujie <1950612658@qq.com>
2020-04-29 21:24:46 +0800

    第二次提交到github上

--- testJAVA/src/com/duyi/testMain.java
+++ testJAVA/src/com/duyi/testMain.java
@@ -2,6 +2,6 @@ package com.duyi;
 
 public class testMain {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello world");
     }
 }

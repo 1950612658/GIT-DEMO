7934babf3fd3b509e351c4334d18b1c77b62d527
sujie <1950612658@qq.com>
2020-04-29 22:00:34 +0800

    test分支 增加hello test'

--- testJAVA/src/com/duyi/testMain.java
+++ testJAVA/src/com/duyi/testMain.java
@@ -2,6 +2,7 @@ package com.duyi;
 
 public class testMain {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello test");
+        System.out.println("hello test'");
     }
 }
